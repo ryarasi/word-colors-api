eb7f719a01119e469bfca93da120e29785018168
ragavyarasi <ragav.code@gmail.com>
2021-10-22 21:40:15 +0530

    Got heroku deployment working

--- api/main.py
+++ api/main.py
@@ -10,9 +10,8 @@ host=str(os.getenv('HOST','0.0.0.0'))
 
 @app.get('/')
 def Home():
-    return "Service is Running...now"
+    return "Service is Running..."
 
 
 if __name__ == "__main__":
-    print('port and reload from env', port, reload)
     uvicorn.run('main:app', port=port, reload=reload, host=host, root_path="/")
