81c438017210cf4f96940d1d4c2116ab8574b845
ragavyarasi <ragav.code@gmail.com>
2022-01-12 02:13:11 +0530

    API working fully

--- api/main.py
+++ api/main.py
@@ -1,15 +1,21 @@
 from fastapi import FastAPI
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
+from ximilar.client import DominantColorProductClient
 import os
+import requests
+
 
 app = FastAPI()
 
 origins = os.getenv('ORIGINS',"*")
 port = int(os.getenv('PORT', 8000))
 reload = bool(os.getenv('RELOAD', 1))
+dev = bool(os.getenv('DEBUG', 0))
 host=str(os.getenv('HOST','0.0.0.0'))
-
+GOOGLE_SEARCH_API_KEY=str(os.getenv('GOOGLE_SEARCH_API_KEY',''))
+GOOGLE_SEARCH_ENGINE_ID=str(os.getenv('GOOGLE_SEARCH_ENGINE_ID',''))
+XIMILAR_API_KEY=str(os.getenv('XIMILAR_API_KEY',''))
 
 app.add_middleware(
     CORSMiddleware,
@@ -19,17 +25,44 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+def get_dominant_colors(imgLink):
+    generic_client = DominantColorProductClient(token=XIMILAR_API_KEY)
+    result = generic_client.dominantcolor([{"_url": imgLink}])
+    return result
+
+
 @app.get('/')
 def Home():
     return "Service is Running..."
 
 @app.get('/images')
-def Images(query):
-    return "Here are the images for the query " + query
+async def Images(query):
+    google_search_api_url = "https://customsearch.googleapis.com/customsearch/v1"+"?q="+query+"&imgColorType=COLOR&imgType=PHOTO&safe=ACTIVE&searchType=image&cx="+GOOGLE_SEARCH_ENGINE_ID+"&key="+GOOGLE_SEARCH_API_KEY
+
+    images_search_response = requests.get(google_search_api_url)
+    json_data = images_search_response.json()
+    resultsExist = int(json_data['searchInformation']['totalResults']) > 0
+    if resultsExist:
+        imageObjects = json_data['items']
+
+        imageLinks = []
+        for item in imageObjects:
+            imageLinks.append(item['link'])
+
+        response = []
+
+        for imageLink in imageLinks:
+            dominant_colors = get_dominant_colors(imageLink)
+            status = dominant_colors['records'][0]['_status']['code']
+            if status == 200:
+                colors = dominant_colors['records'][0]['_dominant_colors']['rgb_hex_colors']
+                response.append({'image': imageLink, 'colors': colors})
+
+        return response
+    else:
+        return images_search_response.json()
+
 
 
 if __name__ == "__main__":
-    print('port => ', port)
-    print('reload => ', reload)
-    print('host => ', host)
     uvicorn.run('main:app', port=port, reload=reload, host=host)
