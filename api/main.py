e8f2728f1578e6b1bec05734da133944d83dffd1
ragavyarasi <ragav.code@gmail.com>
2022-01-11 17:32:53 +0530

    Got dev working

--- api/main.py
+++ api/main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 import os
 
 app = FastAPI()
 
+origins = os.getenv('ORIGINS',"*")
 port = int(os.getenv('PORT', 8000))
 reload = bool(os.getenv('RELOAD', 1))
 host=str(os.getenv('HOST','0.0.0.0'))
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 def Home():
