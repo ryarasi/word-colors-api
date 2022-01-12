9cc47e5301c85deefd6649d98e51ce2289b9e52b
ragavyarasi <ragav.code@gmail.com>
2022-01-12 13:17:58 +0530

    Polished the documentation

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-This is the API for the Word Color app, which takes in a word/phrase from the user and shows a series of colors
+# Word to Colors Generator API
+
+This app takes in a word (or a phrase) as input and shows you associated colors of the object that the word or phrase represents.
+
+Link to app deployed on cloud - https://ragav-word-colors.netlify.app/
+
+Link to the API deployed on the cloud - https://word-colors-api.herokuapp.com
+
+Link to UI code - https://github.com/ryarasi/word-colors
+
+# Basic info
+
+- This project is a dockerized FastAPI-based API with one endpoint that serves the word-colors app linked above.
+- All API secret keys for Google Search API and Ximilar API are stored safely as env variables.
+
+# Notable Packages/Technologies
+
+- [FastAPI](https://fastapi.tiangolo.com/) - A very lightweight, performant and efficient python-based API
+- [Google Custom Search API](https://developers.google.com/custom-search/v1) - This is the primary API that we use to get images associated with search queries
+- [ximilar-client](https://pypi.org/project/ximilar-client/) - A wrapper for the [Ximilar API](https://www.ximilar.com/all-services/) services which offer AI-based visual API services.
+
+# Methodology
+
+- When the API receives the GET request from the client with the search term, we make a request to the Google Custom Search Engine API to request images for that query.
+- We get an array of upto 10 image urls as a response
+- We run these image urls through Ximilar API to get an array of dominant colors for each of those images.
+- The API has means to access the Google Custom Search API, which it uses to search for images for the query.
+- When the images return, we use the [Ximilar API](https://www.ximilar.com/all-services/) package to get an array of dominant colors as hex codes.
+- The API then neatly organizes the image links and the dominant colors for each image into a JSON and sends it back to the client.
+
+# How to run this app on your local machine
+
+- Download this code to your local machine
+- Make sure you have Python 3 installed. [Instructions](https://www.python.org/downloads/)
+- Make sure you have Docker and Docker Compose installed. [Instructions](https://docs.docker.com/compose/install/)
+- Duplicate the `env.sample` file and rename it as `.env` and populate it with the API keys. You can obtain them by visiting the links shown there.
+- Make sure to populate it with the necessary API keys
+- Open your terminal and change directory to the project root folder and enter `npm start` to get started.
+- The API will be served and it can be accessed at `http://0.0.0.0:/8000`
--- api/main.py
+++ api/main.py
@@ -8,6 +8,9 @@ import requests
 
 app = FastAPI()
 
+
+# Getting the necessary variables from the environment variables
+
 origins = os.getenv('ORIGINS',"*")
 port = int(os.getenv('PORT', 8000))
 reload = bool(os.getenv('RELOAD', 1))
@@ -17,6 +20,8 @@ GOOGLE_SEARCH_API_KEY=str(os.getenv('GOOGLE_SEARCH_API_KEY',''))
 GOOGLE_SEARCH_ENGINE_ID=str(os.getenv('GOOGLE_SEARCH_ENGINE_ID',''))
 XIMILAR_API_KEY=str(os.getenv('XIMILAR_API_KEY',''))
 
+# Setting the accepted origins here for CORS
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -25,41 +30,60 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+#  Method to obtain dominant colors from an image URL
 def get_dominant_colors(imgLink):
     generic_client = DominantColorProductClient(token=XIMILAR_API_KEY)
     result = generic_client.dominantcolor([{"_url": imgLink}])
     return result
 
-
+# Placeholder root path
 @app.get('/')
 def Home():
     return "Service is Running..."
 
+# The main url used by the client for the word to colors feature.
+# Takes in a "query" as URL params to do everything
 @app.get('/images')
 async def Images(query):
+    #  We first construct the URL using the variables sourced from the environemnt variables
     google_search_api_url = "https://customsearch.googleapis.com/customsearch/v1"+"?q="+query+"&imgColorType=COLOR&imgType=PHOTO&safe=ACTIVE&searchType=image&cx="+GOOGLE_SEARCH_ENGINE_ID+"&key="+GOOGLE_SEARCH_API_KEY
 
+    # We make a GET request and store the response
     images_search_response = requests.get(google_search_api_url)
+
+    # Convert the response into JSON format
     json_data = images_search_response.json()
+
+    # Check if the results actually exist. Sometimes no results are shown because safe search is active and the query may be NSFW
     resultsExist = int(json_data['searchInformation']['totalResults']) > 0
     if resultsExist:
+
+        # If results exist, then we collect the image objects as an array
         imageObjects = json_data['items']
 
+
+        # Extract only the URLs of the images into a variable
         imageLinks = []
         for item in imageObjects:
             imageLinks.append(item['link'])
 
         response = []
-
+        
+        # Loop through each of those image URLs
         for imageLink in imageLinks:
+            # Get its dominant colors
             dominant_colors = get_dominant_colors(imageLink)
+            # Get the status code of that request to the Ximilar API
             status = dominant_colors['records'][0]['_status']['code']
             if status == 200:
+                # If that request was successful, we proceed to append to the final response, our image URL and its dominant colors
                 colors = dominant_colors['records'][0]['_dominant_colors']['rgb_hex_colors']
                 response.append({'image': imageLink, 'colors': colors})
 
+        # Finally return the response
         return response
     else:
+        # In case we don't get any results we send the original image search response
         return images_search_response.json()
 
 
